9f10251f921fddb4165ea0406d797ebb253ef219
Johnathan Kupferer <jkupfere@redhat.com>
2019-01-18 12:38:36 -0600

    Ansible vault setup

--- provision-controller/ansible-playbook.sh
+++ provision-controller/ansible-playbook.sh
@@ -21,23 +21,16 @@ OPENSHIFT_CLUSTER_DIR=${OPENSHIFT_PROVISION_CONFIG_PATH}/cluster/${OPENSHIFT_PRO
 # Location of openshift-provision-demo to copy over to controller instance
 OPENSHFIT_PROVISION_DEMO_DIR=$(dirname $PWD)
 
-# Vault password file to protect any sensitive configuration
-VAULT_PASSWORD_FILE=${VAULT_PASSWORD_FILE:-$PWD/../.vaultpw}
-
 [[ -d "$OPENSHIFT_PROVISION_CONFIG_PATH" ]] || \
   errexit "OPENSHIFT_PROVISION_CONFIG_PATH not found at $OPENSHIFT_PROVISION_CONFIG_PATH"
 [[ -d "$OPENSHIFT_CLUSTER_DIR" ]] || \
   errexit "Cluster subdirectory not found at $OPENSHIFT_CLUSTER_DIR"
-[[ -f "$VAULT_PASSWORD_FILE" ]] || \
-  errexit "VAULT_PASSWORD_FILE not found at $VAULT_PASSWORD_FILE"
 
 ANSIBLE_VARS="\
 --inventory=../hosts.py \
---vault-password-file=$VAULT_PASSWORD_FILE \
 -e openshift_provision_cluster_name=$OPENSHIFT_PROVISION_CLUSTER_NAME \
 -e openshift_provision_demo_path=$OPENSHFIT_PROVISION_DEMO_DIR \
--e openshift_provision_config_path=$OPENSHIFT_PROVISION_CONFIG_PATH \
--e vault_password_file=$VAULT_PASSWORD_FILE"
+-e openshift_provision_config_path=$OPENSHIFT_PROVISION_CONFIG_PATH"
 
 export ANSIBLE_ROLES_PATH=$OPENSHFIT_PROVISION_DEMO_DIR/roles
 
