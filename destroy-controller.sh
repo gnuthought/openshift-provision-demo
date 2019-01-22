b6ce8efea7410df13e46fbd131f4c8a322222800
Johnathan Kupferer <jkupfere@redhat.com>
2019-01-22 03:12:17 +0000

    Docmentation

---
+++ destroy-controller.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+USAGE="Usage: $0 <OPENSHIFT_PROVISION_CLUSTER_NAME>"
+
+export OPENSHIFT_PROVISION_CLUSTER_NAME=${1:-$OPENSHIFT_PROVISION_CLUSTER_NAME}
+
+errexit () {
+  echo -e "$1\n$USAGE" >&2
+  exit 1
+}
+
+[[ -z "$OPENSHIFT_PROVISION_CLUSTER_NAME" ]] && errexit "No OPENSHIFT_PROVISION_CLUSTER_NAME provided."
+
+cd provision-controller
+./ansible-playbook.sh terraform-destroy.yml $OPENSHIFT_PROVISION_CLUSTER_NAME
+cd ..
